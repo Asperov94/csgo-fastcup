53ab246a06dea3b8d66afe53895695d827c9b66f
Матешев Евгений Дмитриевич <evgeniy.mateshev@intermobility.ru>
2021-02-07 14:53:33 +0300

    w

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
+import os
+import re
+from time import sleep
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask import send_file
+
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-import os
 from bs4 import BeautifulSoup
-from time import sleep
-from flask import send_file
-import re
-import requests
+
+
 
 def csgomapname(csgo_map):
+    """Это докстринг модуля, он однострочный."""
     result = re.findall(r"match(.*?)/", csgo_map)
     result = 'https://csgo.fastcup.net/match' + ''.join(result)
     return result
 
 def csgonamemap(csgo_map):
+    """Это докстринг модуля, он однострочный."""
     driver = webdriver.Remote("http://selenium-chrome:4444/wd/hub", DesiredCapabilities.CHROME)
     #driver = webdriver.Chrome()
     driver.get(csgo_map)
@@ -26,7 +30,8 @@ def csgonamemap(csgo_map):
     driver.quit()
     return soup.findAll("div", {"class": "_2Fhou"})[2].text
 
-def csgo_read(csgo_map, csgonamemap):
+def csgo_read(csgo_map, csgonamemap1):
+    """Это докстринг модуля, он однострочный."""
     #===VARIABLES===
     user=kill=dead=assist=''
     #===---===
@@ -44,45 +49,41 @@ def csgo_read(csgo_map, csgonamemap):
     except OSError:
         pass
     with open("stat.txt", "w") as file:
-    	file.write(main_page)
+        file.write(main_page)
     stat = open('stat.txt', 'r')
     soup = BeautifulSoup(stat, "html.parser")
     user_all = soup.findAll("div", {"class": "_2Q_mv"})
     csv = open('csv.csv', 'w')
     for i in user_all:
         user=i.find("span", class_="_38-ZC")
-        if user == None:
+        if user is None:
             user = 'USER'
         else:
-        	user=i.find("span", class_="_38-ZC").text
+            user=i.find("span", class_="_38-ZC").text
         kill=i.find("div", class_="_3UiPH _2SMgu").text
         dead=i.find("div", class_="_3UiPH _1Fe8v").text
         assist=i.find("div", class_="_3UiPH _26k4_")
-        if assist == None:
+        if assist is None:
             assist = 'A'
         else:
-        	assist=i.find("div", class_="_3UiPH _26k4_").text
-        csv.write(f'{user};{kill};{dead};{assist};{csgonamemap} \n' )
+            assist=i.find("div", class_="_3UiPH _26k4_").text
+        csv.write(f'{user};{kill};{dead};{assist};{csgonamemap1} \n' )
     csv.close()
     stat.close()
     sleep(5)
     driver.quit()
-    
 app = Flask(__name__)
-
 @app.route('/', methods=['GET'])
 def index():
-	return render_template('index.html')
-
+    """Это докстринг модуля, он однострочный."""
+    return render_template('index.html')
 @app.route('/', methods=['POST'])
 def index_post():
-    csmap=''
+    """Это докстринг модуля, он однострочный."""
     csgo_map = request.form.get('csgo_map')
     csgo_map = csgomapname(csgo_map)
     csgonamemap1 = csgonamemap(csgo_map)
-
     csgo_read(csgo_map, csgonamemap1)
     return send_file(os.path.join("." , "csv.csv"), as_attachment=True)
-
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
